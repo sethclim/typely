1eeab57b03fc18615614fa3b6c8490a2032c3df7
sethclim <sethclim@gmail.com>
2025-04-17 22:30:40 -0400

    added resume template display + resume types

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { FileListDisplay } from './components/FIleLIst'
 import { openFile, saveFile } from './services/FileSystem'
+import { ResumeTemplateDisplay } from './components/ResumeTemplateDisplay'
+
+export type Block = {
+  name: string
+  content : string
+}
+
+export type ResumeTemplate = {
+  components : Array<Block>
+}
+
+type ResumeInstance = {
+  template : ResumeTemplate,
+  data : string
+}
 
 function App() {
 
   const [files, setFiles] = useState([])
 
   const [content, setContent] = useState("")
+
+  const [resumeTemplate, setResumeTemplate] = useState({
+    components : [
+      {
+        "name" : "header",
+        "content" : "latex latex latex"
+      },
+      {
+        "name" : "footer",
+        "content" : "latex latex latex"
+      }
+    ]
+  })
 
   const readFileHandler = async() => {
     const res = await openFile()
@@ -19,11 +47,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <FileListDisplay files={files} />
-        <p>{content}</p>
-        <button onClick={() => readFileHandler()}>OPEN</button>
-        <button onClick={() => saveFile("This is a message")}>SAVE</button>
+      <div className='flex flex-row'>
+        {/* <FileListDisplay files={files} /> */}
+        <div>
+          <textarea
+            style={{ width: "100%", height: "300px", marginTop: "1rem" }}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="File content appears here..."
+          />
+          <button onClick={() => readFileHandler()}>OPEN</button>
+          <button onClick={() => saveFile(content)}>SAVE</button>
+        </div>
+        <div>
+          <ResumeTemplateDisplay resumeTemplate={resumeTemplate} />
+        </div>
       </div>
     </>
   )
