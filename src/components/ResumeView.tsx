73e4ce9aa53b38f6a0f2b49f720c455e925f9b2e
sethclim <sethclim@gmail.com>
2025-11-24 18:23:58 -0500

    moved PDFView own file

--- src/components/ResumeView.tsx
+++ src/components/ResumeView.tsx
@@ -1,6 +1,6 @@
 
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { useResume } from '../context/resume/ResumeContext'
 import { ResumeSectionConfigTable, ResumeSectionDataTable } from '../db/tables';
@@ -11,81 +11,16 @@ import ComboBox from './ComboBox';
 import { ResumeTemplateDisplay } from './ResumeTemplateDisplay'
 import { ComponentLibrary, DataItemComponent } from './ComponentLibrary';
 
-import { Document, Page, pdfjs } from "react-pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
-
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
-import { DataItem, ResumeConfig, Template } from '../types';
+import { DataItem, Template } from '../types';
 import { DndContext, DragEndEvent, DragOverlay, DragStartEvent } from '@dnd-kit/core';
 
 // @ts-ignore
 import SyntaxHighlighter from 'react-syntax-highlighter';
 // @ts-ignore
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
-
-type PDFViewProps = {
-  resume : ResumeConfig | null
-}
-
-const PDFView = (props : PDFViewProps) => {
-
-  const [pdfUrl, setPdfUrl] = useState<string | null>(null);
-
-  const compileLatex = async () => {
-    console.log(props.resume?.sections[0].template?.content)
-
-    let latex_string = `\\documentclass[10pt, letterpaper]{article}\\usepackage{config}\\begin{document}`
-    
-    props.resume?.sections.map((section) => {
-      latex_string += section.template?.content
-    })
-
-    latex_string += `\\end{document}`
-    
-    const latexData = { latex: latex_string };
-    const api_url = `${import.meta.env.VITE_BE_URL}/${import.meta.env.VITE_COMPILE_ENDPOINT}`
-    console.log("api_url " + api_url)
-    const response = await fetch(api_url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(latexData),
-    });
-
-    if (!response.ok) {
-      console.error("Failed to compile LaTeX", await response.text());
-      return;
-    }
-
-    // Get PDF as Blob
-    const blob = await response.blob();
-
-    // Create an object URL
-    const url = URL.createObjectURL(blob);
-    setPdfUrl(url);
-  };
-
-  useEffect(()=>{
-    if (props.resume != null)
-      compileLatex()
-  },[props.resume])
-
-  return (
-    <div>
-      {/* <button className='text-white bg-red-500' onClick={compileLatex}>Compile LaTeX</button> */}
-
-    {pdfUrl && (
-      <Document file={pdfUrl}>
-        <Page pageNumber={1} width={600} />
-      </Document>
-    )}
-    </div>
-  );
-}
+import { PDFView } from './PDFView';
 
 export const ResumeView = () => {
   const { resume: myResume } = useResume();
@@ -124,7 +59,7 @@ export const ResumeView = () => {
   
       const [overPrefix, section_id] = over.id.toString().split('-');
       const [activePrefix, active_id] = active.id.toString().split('-');
-      console.log("On Droppable overPrefix:" + overPrefix + " section_id " + section_id + " activePrefix " +  activePrefix  + " active id " + active_id)
+      // console.log("On Droppable overPrefix:" + overPrefix + " section_id " + section_id + " activePrefix " +  activePrefix  + " active id " + active_id)
       
       if (overPrefix === "dataitem" && activePrefix === "dataitem") {
         ResumeSectionDataTable.insert({
@@ -148,9 +83,9 @@ export const ResumeView = () => {
 
       setIsDragging(true);
 
-      const [activePrefix, active_id] = active.id.toString().split('-');
+      const [activePrefix, _] = active.id.toString().split('-');
 
-      console.log("activePrefix " + activePrefix)
+      // console.log("activePrefix " + activePrefix)
 
       if(activePrefix === "dataitem")
       {
