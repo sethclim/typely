03d0f3a9279a6a3227e2c9340b86420c94ed53a9
sethclim <sethclim@gmail.com>
2025-11-23 20:53:21 -0500

    calling compile endpoint and displaying

--- src/components/ResumeView.tsx
+++ src/components/ResumeView.tsx
@@ -11,6 +11,70 @@ import ComboBox from './ComboBox';
 import { ResumeTemplateDisplay } from './ResumeTemplateDisplay'
 import { ComponentLibrary } from './ComponentLibrary';
 
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import { ResumeConfig } from '../types';
+
+type PDFViewProps = {
+  resume : ResumeConfig | null
+}
+
+const PDFView = (props : PDFViewProps) => {
+
+  const [pdfUrl, setPdfUrl] = useState<string | null>(null);
+
+  const compileLatex = async () => {
+    console.log(props.resume?.sections[0].template?.content)
+
+    let latex_string = `\\documentclass{article}\\begin{document}`
+    
+    props.resume?.sections.map((section) => {
+      latex_string += section.template?.content
+    })
+
+    latex_string += `\\end{document}`
+    
+    const latexData = { latex: latex_string };
+    const api_url = `${import.meta.env.VITE_BE_URL}/${import.meta.env.VITE_COMPILE_ENDPOINT}`
+    console.log("api_url " + api_url)
+    const response = await fetch(api_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(latexData),
+    });
+
+    if (!response.ok) {
+      console.error("Failed to compile LaTeX", await response.text());
+      return;
+    }
+
+    // Get PDF as Blob
+    const blob = await response.blob();
+
+    // Create an object URL
+    const url = URL.createObjectURL(blob);
+    setPdfUrl(url);
+  };
+
+  return (
+    <div>
+      <button className='text-white bg-red-500' onClick={compileLatex}>Compile LaTeX</button>
+
+    {pdfUrl && (
+      <Document file={pdfUrl}>
+        <Page pageNumber={1} width={600} />
+      </Document>
+    )}
+    </div>
+  );
+}
+
 export const ResumeView = () => {
   const { resume: myResume } = useResume();
   const [isOpen, setIsOpen] = useState(false);
@@ -48,6 +112,9 @@ export const ResumeView = () => {
           <button onClick={() => setIsOpen(true)}>Add New Component</button>
           </div>
         </div>
+        
+        <PDFView resume={myResume} />
+
          <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
           <h2 className="text-xl font-bold mb-4 text-black">New Resume Block</h2>uik
           <form>
