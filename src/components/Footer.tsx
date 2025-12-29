4774e2dddd223e0870f12bc859c39a3087c295d6
sethclim <sethclim@gmail.com>
2025-12-29 14:56:52 -0500

    styling footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,16 @@ export type FooterProps ={
 
 export const Footer = ({bg = "bg-darkest"} : FooterProps) =>{
     return(
-        <div className={clsx(["flex flex-col w-full justify-center items-center min-h-10 gap-2 p-2 border-t-1 border-grey/20", bg])}>
-            <p className="text-white">@sethclim 2025</p>
+        <div className={clsx(["flex flex-col w-full justify-center items-center min-h-10 p-2 border-t-1 border-grey/20", bg])}>
+            <p className="text-grey">@sethclim 2025</p>
             <ul className="flex flex-row gap-8 text-primary/80 list-disc pl-5">
                 <li>
-                    <Link to="/faq" className="text-primary/80 hover:text-primary brightness-120">
+                    <Link to="/faq" className="text-sm text-primary/80 hover:text-primary brightness-120">
                     FAQ
                     </Link>
                 </li>
                  <li>
-                    <Link to="/pricing" className="text-primary/80 hover:text-primary brightness-120">
+                    <Link to="/pricing" className="text-sm text-primary/80 hover:text-primary brightness-120">
                     PRICING
                     </Link>
                 </li>
