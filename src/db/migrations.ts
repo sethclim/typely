c406d0ce957eceeb1ab3c5660d213a0a31c2af86
sethclim <sethclim@gmail.com>
2026-03-05 12:13:05 -0500

    Added resumeSectionInstance and resumeSectionInstanceData to schema + created migration

--- src/db/migrations.ts
+++ src/db/migrations.ts
@@ -1,3 +1,7 @@
 import migration0000 from '../../drizzle/0000_cooing_goliath.sql?raw'
+import migration0001 from '../../drizzle/0001_breezy_mariko_yashida.sql?raw'
 
-export const migrations = [{ name: '0000_init', sql: migration0000 }]
+export const migrations = [
+	{ name: '0000_init', sql: migration0000 },
+	{ name: '0001_instances', sql: migration0001 }
+]
